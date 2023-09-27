55e11443580d9787dcf46ffc3d102b94184037ce
Ifycode <marychord@gmail.com>
2023-09-27 22:01:58 +0100

    Rotate count from 1 - 5 only

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ displayCount(count);
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        count += 1;
+        count >= 5 ? count = 1 : count += 1 ; //keeps count rotating between number 1 - 5 everytime
         displayCount(count);
         displayColor(btn);
     });
